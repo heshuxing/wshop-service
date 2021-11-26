18198a6f46f6f7a90085068effc0626da6dcec3d
heshuxing <1433762834@qq.com>
2021-11-26 10:17:05 +0800

    完成Dockerfile

---
+++ wshop-core/src/main/java/com/hsx/core/WshopCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hsx.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WshopCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WshopCoreApplication.class, args);
+    }
+
+}
