305050b8e7ce39746cc715c5c45df93bc0ce46c3
heshuxing <1433762834@qq.com>
2021-11-27 17:26:03 +0800

    测试

---
+++ wshop-core/src/main/java/com/hsx/core/controller/HelloContoller.java
@@ -0,0 +1,18 @@
+package com.hsx.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloContoller
+ *
+ * @author shuxing.he
+ * @date 2021/11/27
+ */
+@RestController
+public class HelloContoller {
+    @RequestMapping("/help")
+    public String hello(){
+        return "hello";
+    }
+}
